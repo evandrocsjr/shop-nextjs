3576b50067e254195cc81c4c5d63360812675ae5
evandrocsjr <evandrocunhajunior@gmail.com>
2022-12-04 01:43:40 -0300

    initial commit

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,14 @@
+import {useRouter} from "next/router";
+
+export default function Product() {
+  const { query } = useRouter()
+
+
+
+  return (
+  <div>
+    <h1>Product</h1>
+    <span>{ JSON.stringify(query) }</span>
+  </div>
+  )
+}
